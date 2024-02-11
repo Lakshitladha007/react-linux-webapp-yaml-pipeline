26a2dd7d3f058fc09f502c4264b8db86584bc321
vara prasad reddy jaggu <prasad.reddy0708@gmail.com>
2024-02-11 10:37:36 +0530

    commit8

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>App 7</p>
+        <p>App 8</p>
         <a
           className="App-link"
           href="https://reactjs.org"
