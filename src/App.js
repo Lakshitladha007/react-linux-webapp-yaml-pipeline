fbed3688f75c8b7cda2a63e87d67d9bcd0a490a9
Varaprasad Reddy <prasad.reddy0708@gmail.com>
2024-06-20 12:09:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  console.log(process.env.NODE_ENV);
-  console.log(process.env.REACT_APP_ENVIRONMENT_PROD);
-  console.log(process.env.REACT_APP_ENVIRONMENT_LOCAL);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          App Demo is running in{" "}
-          {process.env.NODE_ENV !== "production"
-            ? process.env.REACT_APP_ENVIRONMENT_LOCAL
-            : process.env.REACT_APP_ENVIRONMENT_PROD}{" "}
-          with github actions self hosted
+          This React App is deployed using Azure YAML Pipelines. The app is running in {process.env.REACT_APP_ENVIRONMENT}
         </p>
         <a
           className="App-link"
